d971fe916805ffb3187271a940c3fda02a6e5ef8
stereo <airstarview@gmail.com>
2024-09-22 14:36:32 +0300

    added html page- show_posts

--- src/pages/router.py
+++ src/pages/router.py
@@ -1,13 +1,12 @@
-from fastapi import APIRouter, Request, Form, Depends,File,UploadFile
+import base64
+from fastapi import APIRouter, Request, Form, Depends, File, UploadFile
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse,PlainTextResponse
+from fastapi.responses import HTMLResponse, PlainTextResponse
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_async_session
 from src.posts.models import Posts
-
-
-
 
 router = APIRouter(
     prefix="/pages",
@@ -15,20 +14,24 @@ router = APIRouter(
 )
 
 templates = Jinja2Templates(directory="src/templates")
+def b64encode(value: bytes) -> str:
+    return base64.b64encode(value).decode('utf-8')
 
-# Обработчик для получения формы
+templates.env.filters['b64encode'] = b64encode
+
+
 @router.get("/create_post", response_class=HTMLResponse)
 async def get_create_post(request: Request):
     return templates.TemplateResponse('create_post.html', {"request": request})
 
-# Обработчик для создания нового поста
+
 @router.post("/create_post", response_class=HTMLResponse)
 async def create_new_post(
-    request: Request,
-    title: str = Form(...),
-    tags: str = Form(...),
-    photo: UploadFile = File(...),
-    session: AsyncSession = Depends(get_async_session)
+        request: Request,
+        title: str = Form(...),
+        tags: str = Form(...),
+        photo: UploadFile = File(...),
+        session: AsyncSession = Depends(get_async_session)
 ):
     photo_data = await photo.read()
 
@@ -36,10 +39,19 @@ async def create_new_post(
         text=title,
         tags=tags.split(','),
         photo=photo_data,
-        user_id=1  # Замените на актуальный ID пользователя
+        user_id=1
     )
 
     session.add(new_post)
     await session.commit()
 
     return templates.TemplateResponse('create_post.html', {"request": request, "message": "Post successfully created!"})
+
+
+@router.get("/show_posts", response_class=HTMLResponse)
+async def show_posts(request: Request, session: AsyncSession = Depends(get_async_session)):
+    query = select(Posts)
+    result = await session.execute(query)
+    posts = result.scalars().all()
+
+    return templates.TemplateResponse('show_posts.html', {"request": request, "posts": posts})
